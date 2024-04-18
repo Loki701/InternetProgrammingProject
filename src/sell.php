8b75181dd47e94557cf6d8c50286d4c46aed6369
Kevin Andrew Daniel <kevindaniel@storm.cise.ufl.edu>
2024-04-17 21:32:18 -0400

    redesign listing page

--- src/sell.php
+++ src/sell.php
@@ -1,21 +1,21 @@
 #!/usr/local/bin/php
 <?php
-session_start();
-require_once("utilities/database.php");
-require_once("utilities/utility_functions.php");
-if (!check_login()) {
-    header("Location: login.php");
-    die;
-}
+    session_start();
+    require_once("utilities/database.php");
+    require_once("utilities/utility_functions.php");
+    if (!check_login()) {
+        header("Location: login.php");
+        die;
+    }
 
-$userID = $_SESSION['user_ID'];
+    $userID = $_SESSION['user_ID'];
 
-$connection = connect();
+    $connection = connect();
 
-$result = getAllEvents($connection);
-$row = mysqli_fetch_assoc($result);
-$firstEventName = $row['EventName'];
-$firstEventID = $row['EventID'];
+    $result = getAllEvents($connection);
+    $row = mysqli_fetch_assoc($result);
+    $firstEventName = $row['EventName'];
+    $firstEventID = $row['EventID'];
 ?>
 
 <!DOCTYPE html>
@@ -34,27 +34,27 @@ $firstEventID = $row['EventID'];
     <link type="text/css" rel="stylesheet" href="./css/sell.css">
 
     <?php
-        if ($_POST['action']) {
-            if ($_POST['action'] == 'addListing') {
-                $negotiable = isset($_POST['negotiable']) ? "1" : "0";
-                addListing($connection, $userID, $_POST['game'], $_POST['price'], $_POST['section'], $_POST['row'], $_POST['seat'], $negotiable);
-                header('Location: profile.php');
-            }
-            $_POST = array();
+    if ($_POST['action']) {
+        if ($_POST['action'] == 'addListing') {
+            $negotiable = isset($_POST['negotiable']) ? "1" : "0";
+            addListing($connection, $userID, $_POST['game'], $_POST['price'], $_POST['section'], $_POST['row'], $_POST['seat'], $negotiable);
+            header('Location: profile.php');
         }
+        $_POST = array();
+    }
     ?>
     <script>
         $(document).ready(function() {
             $('a[name=games]').click(function() {
                 $("#dropdownMenuButton").html($(this).html());
                 $("#game").val($(this).attr('value'));
-            });          
+            });
         });
-        </script>
+    </script>
 
 </head>
 
-<body class="bg-light">
+<body>
     <!-- Navbar -->
     <div id="nav-placeholder"></div>
     <script>
@@ -65,77 +65,92 @@ $firstEventID = $row['EventID'];
             });
         });
     </script>
-    <div class="container bg-light pt-3 pb-3">
-        <div class="row justify-content-center">
-            <h2>Post your ticket for sale</h2>
-            <br><br><br>
-        </div>
-        <div class="row col-md-4 bg-white p-4 rounded-xl offset-md-4 justify-content-center">
-            <form action="sell.php" method="post">
-                <input type="hidden" name="action" value="addListing">
+    <div class="container bg">
+        <div class="row justify-content-center w-100">
+            <div class="col-xl-10 ">
+                <div class="card border-0">
+                    <div class="card-body p-0">
+                        <div class="row no-gutters">
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <h3 class="h4 font-weight-bold text-theme mb-5">List Ticket</h3>
+                                    <p class="text-muted mt-2 mb-5">Enter details about your ticket below to list your ticket.</p>
 
-                <div class="form-group row">
-                    <label for="dro" class="col-sm-3 col-form-label">Game</label>
-                    <div class="col-sm-7 offset-sm-1">
-                        <div id="dropdown" class="dropdown">
-                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <?php echo($firstEventName)?>
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <?php
-                                $result = getAllEvents($connection);
-                                while ($row = mysqli_fetch_assoc($result)) {
-                                    echo "<a class='dropdown-item' href='javascript:void(0);' name='games' value=" . $row['EventID'] . ">" . $row['EventName'] . "</a>";
-                                }
-                                ?>
+                                    <form action="sell.php" method="post">
+                                        <input type="hidden" name="action" value="addListing">
+
+                                        <div class="form-group row">
+                                            <label for="dro" class="col-sm-3 col-form-label">Game</label>
+                                            <div class="col-sm-7 offset-sm-1">
+                                                <div id="dropdown" class="dropdown">
+                                                    <button class="btn btn-theme dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                        <?php echo ($firstEventName) ?>
+                                                    </button>
+                                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                        <?php
+                                                        $result = getAllEvents($connection);
+                                                        while ($row = mysqli_fetch_assoc($result)) {
+                                                            echo "<a class='dropdown-item' href='javascript:void(0);' name='games' value=" . $row['EventID'] . ">" . $row['EventName'] . "</a>";
+                                                        }
+                                                        ?>
+                                                    </div>
+                                                    <?php
+                                                    echo ('<input type="hidden" id="game" name="game" required value="' . $firstEventID . '">')
+                                                    ?>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="section" class="col-sm-3 col-form-label">Section</label>
+                                            <div class="col-sm-7 offset-sm-1">
+                                                <input type="number" min="23" max="44" class="form-control" id="section" name="section" placeholder="32" required>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="row" class="col-sm-3 col-form-label">Row</label>
+                                            <div class="col-sm-7 offset-sm-1">
+                                                <input type="number" min="0" class="form-control" id="row" name="row" placeholder="4" required>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="seat" class="col-sm-3 col-form-label">Seat</label>
+                                            <div class="col-sm-7 offset-sm-1">
+                                                <input type="number" min="0" class="form-control" id="seat" name="seat" placeholder="17" required>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="price" class="col-sm-3 col-form-label">Price</label>
+                                            <div class="col-sm-7 offset-sm-1">
+                                                <input type="number" step="0.01" min="0" class="form-control" id="price" name="price" placeholder="20.00" required>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="negotiable" class="col-sm-4 col-form-label">Negotiable?</label>
+                                            <div class="col-sm-2">
+                                                <input style="accent-color: #5369f8;" type="checkbox" class="form-control" id="negotiable" name="negotiable">
+                                            </div>
+                                        </div>
+                                        <br>
+                                        <div class="form-group row">
+                                            <div class="col-sm-4">
+                                                <input class="btn btn-theme" type="submit" value="Confirm">
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
-                            <?php
-                            echo('<input type="hidden" id="game" name="game" required value="' . $firstEventID . '">')
-                            ?>
-                            
+
+                            <div class="col-lg-6 d-none d-lg-inline-block">
+                                <div class="account-block rounded-right">
+                                    <div class="overlay rounded-right"></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div class="form-group row">
-                    <label for="section" class="col-sm-3 col-form-label">Section</label>
-                    <div class="col-sm-7 offset-sm-1">
-                        <input type="number" min="23" max="44" class="form-control" id="section" name="section" placeholder="32" required>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="row" class="col-sm-3 col-form-label">Row</label>
-                    <div class="col-sm-7 offset-sm-1">
-                        <input type="number" min="0" class="form-control" id="row" name="row" placeholder="4" required>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="seat" class="col-sm-3 col-form-label">Seat</label>
-                    <div class="col-sm-7 offset-sm-1">
-                        <input type="number" min="0" class="form-control" id="seat" name="seat" placeholder="17" required>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="price" class="col-sm-3 col-form-label">Price</label>
-                    <div class="col-sm-7 offset-sm-1">
-                        <input type="number" step="0.01" min="0" class="form-control" id="price" name="price" placeholder="20.00" required>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="negotiable" class="col-sm-3 col-form-label">Negotiable?</label>
-                    <div class="col-sm-2 offset-sm-1">
-                        <input type="checkbox" class="form-control" id="negotiable" name="negotiable">
-                    </div>
-                </div>
-                <br>
-                <div class="form-group row justify-content-center">
-                    <div class="col-sm-4">
-                        <input class="btn btn-primary" type="submit" value="Confirm">
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </body>
-
 </html>
